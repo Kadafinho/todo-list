705daa028388179ca632243f4d2f83fb2aff072d
Diogo Rafael Soares <68117@PROWAY.TREINA>
2021-02-10 20:43:42 -0300

    estrutura inicial todo-list

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public todos : any[] = [];
+  public title : string = 'Minhas tarefas';
+
+  constructor() {
+
+    
+
+    this.todos.push('passear com cachorro');
+    this.todos.push('ir ao super mercado');
+    this.todos.push('cortar cabelo');
+
+  }
+
+  alterarTexto(){
+    this.title = 'Texto alterado'
+  }
+}

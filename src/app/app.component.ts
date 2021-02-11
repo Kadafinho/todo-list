5e10048b2223f93effcdaa59b9e6444fd8870dbd
Diogo Rafael Soares <68117@proway.treina>
2021-02-11 20:07:54 -0300

    adicionando as acoes nos botoes refazer e concluir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,28 @@ export class AppComponent {
 
   }
 
+
+remove (todo : Todo){
+  const index = this.todos.indexOf(todo);
+  if (index !== -1){
+    // verifica se o todo esta na lista, se for !== -1 está na lista,
+      // ai pode remover
+    this.todos.splice(index, 1);
+    // pega o item que vai ser removido e diz quantos item precisa remover
+  }
+}
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
+
   alterarTexto(){
     this.title = 'Texto alterado'
   }
 }
+
+
